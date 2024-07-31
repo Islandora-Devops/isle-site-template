5390b353f6819325524bda808f778e94c7d2d10f
Joe Corall <jjc223@lehigh.edu>
2024-07-31 13:22:56 -0400

    Add a solr test (#52)

--- tests/init-template-starter.sh
+++ tests/init-template-starter.sh
@@ -10,12 +10,15 @@ if [ ! -v ISLANDORA_TAG ] || [ "$ISLANDORA_TAG" = "" ]; then
   ISLANDORA_TAG=main
 fi
 
+# save the site template default settings.php
+# so we can overwrite it for the starter site
 mv drupal/rootfs/var/www/drupal/assets/patches/default_settings.txt .
-
 curl -L "https://github.com/Islandora-Devops/islandora-starter-site/archive/refs/${ISLANDORA_STARTER_REF}.tar.gz" \
   | tar --strip-components=1 -C drupal/rootfs/var/www/drupal -xz
-
 mv default_settings.txt drupal/rootfs/var/www/drupal/assets/patches/default_settings.txt
+
+# copy any drush scripts into the rootfs so we can run them
+cp ./tests/solr.php drupal/rootfs/var/www/drupal/
 
 ./generate-certs.sh
 ./generate-secrets.sh
@@ -23,3 +26,5 @@ mv default_settings.txt drupal/rootfs/var/www/drupal/assets/patches/default_sett
 docker compose --profile dev up -d
 
 ./tests/ping.sh
+
+docker compose --profile dev exec drupal-dev drush scr solr.php
