c66911135de118e43c29e6d845f9f6d83baad400
Joe Corall <jjc223@lehigh.edu>
2024-10-04 09:03:54 -0400

    debug

--- tests/ping.sh
+++ tests/ping.sh
@@ -12,6 +12,7 @@ while true; do
   ((COUNTER++))
   if [ "${COUNTER}" -eq 50 ]; then
     echo "Failed to come online after 4m"
+    docker logs isle-site-template-drupal-dev-1
     exit 1
   fi
   sleep 5;
