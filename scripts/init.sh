c6d9c1478a695f65096104d623075b7c30f60890
Joe Corall <joe@libops.io>
2026-01-11 17:05:54 -0500

    fix selinux edge

--- scripts/init.sh
+++ scripts/init.sh
@@ -12,6 +12,18 @@ fi
 
 id -u > ./certs/UID
 
+
+# For SELinux if applicable.
+if command -v "sestatus" >/dev/null; then
+  if sestatus | grep -q "SELinux status: *enabled"; then
+    if command -v "chcon" >/dev/null; then
+      PROGDIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd | xargs dirname)"
+      sudo chcon -R -t container_file_t "${PROGDIR}/secrets" || true
+      sudo chcon -R -t container_file_t "${PROGDIR}/certs" || true
+    fi
+  fi
+fi
+
 docker compose run --rm init
 
 chown -R "$(whoami)" ./certs ./secrets || sudo chown -R "$(whoami)" ./certs ./secrets
